bef204ac3a3bedb699627eaaa2067b1ac1dcf583
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-01 12:40:25 +0800

    update pom.xml file info

--- spring-boot-kotlin-blog/src/main/kotlin/com/laibao/kotlin/springboot/blog/KotlinSpringBootBlogApplication.kt
+++ spring-boot-kotlin-blog/src/main/kotlin/com/laibao/kotlin/springboot/blog/KotlinSpringBootBlogApplication.kt
@@ -1,9 +1,12 @@
 package com.laibao.kotlin.springboot.blog
 
+import com.laibao.kotlin.springboot.blog.configuration.BlogProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class KotlinSpringBootBlogApplication {
 
     companion object{
