56d0550f5d77ecf954b5f5594f2a19b268a8104a
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-01 14:30:05 +0800

    update pom.xml file info

---
+++ spring-boot-kotlin-servletstack/src/main/kotlin/com/laibao/kotlin/springboot/servletstack/KotlinSpringBootServletStackApplication.kt
@@ -0,0 +1,18 @@
+package com.laibao.kotlin.springboot.servletstack
+
+import com.laibao.kotlin.springboot.blog.configuration.BlogProperties
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
+class KotlinSpringBootServletStackApplication {
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<KotlinSpringBootServletStackApplication>(*args)
+        }
+    }
+}
